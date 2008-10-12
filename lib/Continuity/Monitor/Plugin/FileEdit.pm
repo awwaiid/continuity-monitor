b0de50552220e6433153bc05411f13b688b405d8
awwaiid <awwaiid@thelackthereof.org>
2008-10-12 14:33:38 -0400

    Updates to FileEdit
    darcs-hash:20081012183338-4fc8e-4ea2fa636dfb23ff5e81b55155598bf5bfdebe5a.gz

--- lib/Continuity/Monitor/Plugin/FileEdit.pm
+++ lib/Continuity/Monitor/Plugin/FileEdit.pm
@@ -13,10 +13,11 @@ method main {
     $self->print("<div class=dialog title='File Edit'>");
     my @files = sort values %INC;
     @files = grep !/home/, @files;
-    @files = map { "<li>" . $self->cb_link( $_ => sub {
-      $self->edit_file($_);
+    @files = map { my $f = $_; "<li>" . $self->cb_link( $f => sub {
+      $self->edit_file($f);
     }) . "</li>" } @files;
     $self->print("<ul>@files</ul>");
+    $self->print($self->cb_button('gah'   => sub { print STDERR "Gah\n" }));
     $self->print("</div>");
     $self->yield(1);
   }
@@ -27,15 +28,17 @@ method edit_file($filename) {
   $content = encode_entities($content);
   my $textarea_name = $self->field_name('content');
   while(1) {
-    my $action;
+    my $action = '';
     $self->print(qq|
-      <div class=dialog title='File Edit'>");
+      <div class=dialog title='File Edit'>
         <textarea style="width: 100%; height: 30em;" name="$textarea_name">$content</textarea>
-        | . $self->cb_button('Cancel' => sub { $action = 'cancel' })
+        | . $self->cb_button('Cancel' => sub { print STDERR "Got cancel\n"; $action = 'cancel' })
         .   $self->cb_button('Save'   => sub { $action = 'save' })
+        .   $self->cb_link('gah'   => sub { print STDERR "Gah\n" })
         . '</div>'
     );
     $self->yield(1);
+    print STDERR "Action: $action\n\n";
     if($action eq 'save') {
       $content = $self->param($textarea_name);
       write_file($filename);
