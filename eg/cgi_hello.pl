6039814468b47985cd50f7c701ed2f121046a8eb
awwaiid <awwaiid@thelackthereof.org>
2008-10-13 23:55:33 -0400

    Simplify cgi example
    darcs-hash:20081014035533-4fc8e-5fc027d7acfb4556fff056723a9e5e2eb709d12a.gz

--- eg/cgi_hello.pl
+++ eg/cgi_hello.pl
@@ -3,12 +3,9 @@
 use strict;
 use lib '../lib';
 use Continuity::Monitor::CGI qw( inspect );
+print "Content-type: text/html\n\n";
 
 my $x = 5;
-
-sub print_header {
-  print "Content-type: text/html\n\n";
-}
 
 sub print_page {
   my $y = 23;
@@ -27,11 +24,8 @@ sub print_hello {
   print "world!";
 }
 
-#chdir('..');
-print_header();
 print_page();
 
 print "<br>x=$x<br><br>\n";
 print "Goodbye!\n";
-
 
